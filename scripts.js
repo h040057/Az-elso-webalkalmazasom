2e810c813a5e77d09490877770a49d0ef3f10649
XX <h040057@gmail.com>
2021-08-15 21:26:38 +0200

    Az első webalkalmazásom

---
+++ scripts.js
@@ -0,0 +1,121 @@
+var imagesData = [
+    {
+        photo: "images/IMG_0009.jpg",
+        thumb: "images/thumbnails/IMG_0009_thumbnail.jpg",
+        title: "A schönbrunni kastély oldalában lévő kert",
+        description: "A Schönbrunni kastély Európa egyik legszebb barokk épületegyüttese. A Habsburgok hajdani nyári rezidenciája pompás császári dísztermekkel, és egy pazar parkkal nyűgöz le. A Schönbrunni kastélyban lakott valamikor Mária Terézia, Ferenc József császár, Erzsébet császárné, és a császári család számos más tagja."
+    },
+    {
+        photo: "images/IMG_0378.jpg",
+        thumb: "images/thumbnails/IMG_0378_thumbnail.jpg",
+        title: "London Eye",
+        description: "A London Eye más néven Millenniumi Kerék egy óriáskerék Londonban. 1999-ben készült el, a nagyközönség számára 2000 márciusában nyitották meg. A London Eye 135 m magas, és a Jubilee Gardens nyugati, a Temze keleti partján, Lambethben fekszik, a Westminster és a Hungerford híd között. 2006 májusáig ez volt a világ legmagasabb óriáskereke."
+    },
+    {
+        photo: "images/IMG_0514.jpg",
+        thumb: "images/thumbnails/IMG_0514_thumbnail.jpg",
+        title: "Tower Bridge",
+        description: "A Tower Bridge (vagy Tower híd) közúti híd a Temze fölött, London északi és déli felét köti össze. London számára a tengeri kapcsolat nagyon fontosabb volt, így a parlament előírta, hogy a hídnak biztosítania kell a legnagyobb tengeri hajók áthaladásának lehetőségét is. Ez az előírás vezetett a felnyitható középső nyíláshoz, ahol a középső nyílást egy kétszárnyú billenőhíd alkotja. Nevét a közelben található londoni Towerről kapta, megjelenését is hozzá kellett igazítani, így jöttek létre a ma is látható stílusú tornyok. Építését 1886-ban kezdték el és 1894-ben fejezték be. "
+    },
+    {
+        photo: "images/IMG_0628.jpg",
+        thumb: "images/thumbnails/IMG_0628_thumbnail.jpg",
+        title: "A Hampton Court-palota déli homlokzata",
+        description: "A Hampton Court-palota (Hampton Court Palace) egy angol királyi palota Londonban, Richmond upon Thames kerületben, a város központjától 18.5 kilométerre délnyugati irányban a Temze partján. A Tudor-kori építészet gyöngyszeme, amely VIII. Henrik kedvenc lakhelyeként vonult be a köztudatba, de valójában korábban épült. 1689-től a palota Wren barokk stílusú munkásságának is jelentős állomása volt, így az építkezés jellege két építészeti stílus korszakát tükrözi."
+    },
+    {
+        photo: "images/IMG_0752.jpg",
+        thumb: "images/thumbnails/IMG_0752_thumbnail.jpg",
+        title: "A Buckingham-palota madártávlatból",
+        description: "A Buckingham-palota (Buckingham Palace) a brit uralkodó egyik rezidenciája, lakhelye. A palota London belvárosában, Westminster városrészben található. Az épület állami alkalmak, uralkodói rendezvények helyszíne, valamint közkedvelt turistacélpont. A történelem során a palota a britek gyülekezőhelyeként szolgált ünnepek, vagy válsághelyzetek alkalmával. "
+    },
+    {
+        photo: "images/IMG_0756.jpg",
+        thumb: "images/thumbnails/IMG_0756_thumbnail.jpg",
+        title: "A Westminster-palota madártávlatból",
+        description: "A Westminster-palota az Egyesült Királyság parlamentjének épülete, ahol a parlament két háza, a Lordok Háza és a Képviselőház tartja üléseit. A palota Londonban, a Temze északi partján található a City of Westminster kerület szívében, a westminsteri apátság és a kormányzati negyed (a Whitehall és a Downing Street) közelében. Óratornya, mely fő harangja után a Big Ben néven vált híressé, London és általában Anglia egyik legmeghatározóbb jelképe, a város egyik legnépszerűbb turistalátványossága. A Westminster-palota 1970 óta elsőrendű műemlék és 1987 óta a világörökség része."
+    },
+    {
+        photo: "images/IMG_0865.jpg",
+        thumb: "images/thumbnails/IMG_0865_thumbnail.jpg",
+        title: "A British Museum tetőszerkezete",
+        description: "A londoni British Museum egyike a világ legnagyobb, az emberi történelemmel és kultúrával foglalkozó múzeumainak. Gyűjteményei – melyek együttesen több mint 13 millió tárgyból állnak – a kezdetektől a jelenkorig mutatják be az emberi kultúra történetét. A British Museumot 1753-ban alapították, nagyrészt Sir Hans Sloane orvos és tudós gyűjteményére támaszkodva. A múzeum először 1759. január 15-én nyitotta meg kapuit a közönségnek a bloomsbury-i Montague House-ban, a mai múzeumépület helyén."
+    },
+    {
+        photo: "images/IMG_1085.jpg",
+        thumb: "images/thumbnails/IMG_1085_thumbnail.jpg",
+        title: "Párizsi Champs-Élysées",
+        description: "A Champs-Élysées, teljes nevén Avenue des Champs-Élysées a francia főváros, Párizs legismertebb és legelőkelőbb sugárútja. A Champs Élysées a város tengelye. 70 méter széles, hossza a Concorde tértől a Diadalívig 1910 méter. Az útpálya 2x4 sávos és két oldalát sétányok szegélyezik, melyeket fasorok osztanak két részre."
+    },
+    {
+        photo: "images/IMG_1149.jpg",
+        thumb: "images/thumbnails/IMG_1149_thumbnail.jpg",
+        title: "Párizs éjjel az Eiffel-toronyból",
+        description: "Az Eiffel-torony Párizs egyik jelképe. Nevét a tervezőcég tulajdonosáról, Gustave Eiffel mérnökről kapta. Az 1889. évi világkiállításra készült, az eredeti tervek szerint a kiállítás után lebontották volna. A torony összmagassága 324 m, ebből maga a torony 300 m, rászerelve 24 m-re magasodik egy tévé-adóantenna. Első emelete 58, második emelete 116, a harmadik 276 m magasban van. Négyzetalapú, 1,6 ha alapterületű, 10 100 tonna tömegű, 12 000 acéldarabból, szegecseléssel állították össze. A New York-i Chrysler Building (319 m) 1930-as felépítéséig a legmagasabb épület volt a világon."
+    },
+    {
+        photo: "images/IMG_1169.jpg",
+        thumb: "images/thumbnails/IMG_1169_thumbnail.jpg",
+        title: "Disneyland (Párizs)",
+        description: "Disneyland a The Walt Disney Company által 1992-ben létrehozott szórakoztató park. Franciaországban, Párizsban található.  A parkban sétáló Walt Disney figurákon kívül a Kalandország (Adventureland) részen találkozhatunk többek között a Karib-tenger kalózai alakjaival, felülhetünk az Indiana Jones filmjéből megismert hullámvasútra. 2020. július 15-én újra kinyitott."
+    }
+]
+
+var currentPhoto = 0
+var activeIndex = currentPhoto
+
+var loadPhoto = function(currentPhoto) {
+    $("#photo").attr("src", imagesData[currentPhoto].photo)
+    $("#photo-title").text(imagesData[currentPhoto].title)
+    $("#photo-description").text(imagesData[currentPhoto].description)
+}
+
+function loadThumbnails(itemData, index) {
+    $("#thumbnails-container").append(
+        `<div class="thumbnail-box">
+            <img class="thumbnail" data-idx="${index}" src="${itemData.thumb}">
+            <p class="title">${itemData.title}</p>
+        </div>`
+    )
+}
+
+loadPhoto(currentPhoto)
+imagesData.forEach(loadThumbnails)
+$(`.thumbnail[data-idx="${activeIndex}"]`).css({"box-shadow": "0 4px 8px black"})
+
+$("#left-arrow").click(function() {
+    activeIndex = currentPhoto
+    if(currentPhoto > 0) {
+        currentPhoto--
+    } else {
+        currentPhoto = imagesData.length - 1
+    }
+    loadPhoto(currentPhoto)
+    activeThumbnail(activeIndex)
+})
+
+$("#right-arrow").click(function() {
+    activeIndex = currentPhoto
+    if(currentPhoto < imagesData.length - 1) {
+        currentPhoto++
+    } else {
+        currentPhoto = 0
+    }
+    loadPhoto(currentPhoto)
+    activeThumbnail(activeIndex)
+})
+
+$(".thumbnail").click(function(event) {
+    activeIndex = currentPhoto
+    var idxClicked = $(event.target).attr("data-idx")
+    var idxNumber = parseInt(idxClicked)
+    currentPhoto = idxNumber
+    activeThumbnail(activeIndex)
+    loadPhoto(currentPhoto)
+})
+
+function activeThumbnail (activeIndex) {
+    $(`.thumbnail[data-idx="${activeIndex}"]`).removeAttr("style")
+    activeIndex = currentPhoto
+    $(`.thumbnail[data-idx="${activeIndex}"]`).css("box-shadow", "0 4px 8px black")
+}
